6e13cf736b5508ca0a425a6b6716146d5b41bc48
Siddhant Chourasia <53994891+siddhant48@users.noreply.github.com>
2021-07-06 15:22:31 +0530

    Create Longest Common Subsequence.cpp

---
+++ Longest Common Subsequence.cpp
@@ -0,0 +1,21 @@
+//Given two sequences, find the length of longest subsequence present in both of them. Both the strings are of uppercase.
+
+int lcs(int x, int y, string s1, string s2){
+    
+    // your code here
+    int dp[x+1][y+1];
+    memset(dp,0,sizeof(dp));
+    for(int i=0;i<=x;i++)
+    {
+        for(int j=0;j<=y;j++)
+        {
+            if(i==0 || j==0)
+             dp[i][j]=0;
+             else if(s1[i-1]==s2[j-1])
+             dp[i][j]=dp[i-1][j-1]+1;
+             else dp[i][j]=max(dp[i-1][j-1],max(dp[i-1][j],dp[i][j-1]));
+        }
+    }
+    return dp[x][y];
+    
+}
